c5e8b1f0d7b7cdf1c86ebec135103c6da67c0276
46184597 <weichia.su@students.mq.edu.au>
2021-10-05 17:40:25 +0800

    middleware

---
+++ server/utils/middleware.js
@@ -0,0 +1,16 @@
+const requestLogger=(request,response,next)=>{
+    console.log(request.method,response.path)
+    console.log(request.body)
+    console.log('---')
+    next()
+}
+
+
+const errorMiddleware=(error,request,response,next)=>{
+    console.log("ERROR",error.name)
+    response.status(500).send({error:error.name})
+    next()
+}
+
+
+module.exports={requestLogger,errorMiddleware}
