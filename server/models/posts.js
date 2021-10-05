b050ca12a55f0faeae86d2ef22002d1c47fc6b4d
46184597 <weichia.su@students.mq.edu.au>
2021-10-05 17:40:05 +0800

    mongodb

---
+++ server/models/posts.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const url=process.env.MONGODB_URI
+
+console.log('connecting to',url)
+
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(result=>{
+    console.log('connected to MongoDB')
+})
+.catch((error)=>{
+    console.log('error connected to MongoDB',error.message)
+})
+
+const postSchema=new mongoose.Schema({
+    title:String,
+    likes:Number,
+    comments:Array
+})
+
+postSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+const Post=mongoose.model("Post",postSchema)
+
+module.exports=Post
