333f375f781ae53aa42df899619f868ff05a3f96
46184597 <weichia.su@students.mq.edu.au>
2021-10-21 18:18:39 +0800

    add user to post and change interface

--- src/Components/postList.js
+++ src/Components/postList.js
@@ -23,18 +23,32 @@ const Post = ({post, deleteFn,addLike}) => {
       setNum(num+1)
       fetchPost(num);
   }
-
+ 
+ if(window.location.pathname.includes("Mypost")){
   return (
-      <div>
-        
-        <h3 key={post.id}>
-            <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </h3>
-        <h5>{post.author}</h5>
-        <button onClick={()=> addLike(post)}>Like {post.likes} </button> 
-        <button onClick={() => deleteFn(post)}>Delete</button>
-      </div>
-  )
+    <div> 
+      <img width="80px" height="70px"  src={post.image_url}></img>
+      <h3 key={post.id}>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <h5>{post.author}</h5>
+      <button onClick={()=> addLike(post)}>Like {post.likes} </button> 
+      <button onClick={() => deleteFn(post)}>Delete</button>
+    </div>
+)
+ }else{
+  return (
+    <div> 
+      <img width="80px" height="70px"  src={post.image_url}></img>
+      <h3 key={post.id}>
+          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <h5>{post.author}</h5>
+      <button onClick={()=> addLike(post)}>Like {post.likes} </button> 
+    </div>
+)
+ }
+ 
 
 }
 
